c2ecc90804b995eb783e1e8479ca5f133226facb
virginiarm <92397676+virginiarm@users.noreply.github.com>
2025-04-23 02:40:00 -0400

    finishing lesson 3

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,12 +1,20 @@
+import React from 'react';
+import TodoListItem from './TodoListItem';
+
 function TodoList({ todoList = [] }) {
+    const todos=[
+        { id: 1, title: 'Buy groceries' },
+    { id: 2, title: 'Walk the dog' },
+    { id: 3, title: 'Do laundry' }
+  ];
     return (
       <ul>
-        {todoList.map(todo => (
-          <li key={todo.id}>{todo.title}</li>
+        {todos.map(todo => (
+          <TodoListItem key={todo.id} todo={todo}/>
         ))}
       </ul>
     );
-  }
+}
   
   export default TodoList;
   
